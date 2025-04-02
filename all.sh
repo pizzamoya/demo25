ca90e8537c15ead33f54c612f6f522dfaab1a3a4
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-04-02 16:33:13 +0400

    half br-srv

--- all.sh
+++ all.sh
@@ -95,12 +95,47 @@ logdir /var/log/chrony
 # Select which information is logged.
 #log measurements statistics tracking
 EOF
+else
+echo "this is not HQ-SRV" 
+fi
 
 
 
 
-
-
+if [ "$HOSTNAME" = BR-SRV.au-team.irpo ]; then
+useradd sshuser -u 1010
+echo -e "P@ssw0rd\nP@ssw0rd" | passwd sshuser
+usermod -aG wheel sshuser
+cat <<EOF >> /etc/sudoers
+sshuser ALL=(ALL) NOPASSWD:ALL
+EOF
+sed -i 's/#Port 22/Port 2024/' /etc/openssh/sshd_config
+sed -i 's/#MaxAuthTries 6/MaxAuthTries 2/' /etc/openssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/openssh/sshd_config
+cat <<EOF >> /etc/openssh/sshd_config 
+AllowUsers sshuser
+EOF
+cat << EOF >> /etc/openssh/bannermotd
+----------------------
+Authorized access only
+----------------------
+EOF
+systemctl restart sshd
 apt-get install chrony -y 
 sed -i '3i#pool pool.ntp.org iburst' /etc/chrony.conf
 systemctl enable --now chronyd
+cat <<EOF >> /etc/resolv.conf 
+nameserver 8.8.8.8
+EOF
+apt-get update && apt-get install -y task-samba-dc bind 
+control bind-chroot disabled
+grep -q KRB5RCACHETYPE /etc/sysconfig/bind || echo 'KRB5RCACHETYPE="none"' >> /etc/sysconfig/bind
+systemctl stop bind
+rm -f /etc/samba/smb.conf
+rm -rf /var/lib/samba
+rm -rf /var/cache/samba
+mkdir -p /var/lib/samba/sysvol
+samba-tool domain provision --realm=au-team.irpo --domain=au-team --adminpass='P@ssw0rd' --dns-backend=SAMBA_INTERNAL --server-role=dc --use-rfc2307
+
+
+
