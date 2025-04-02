e16a314db299ac27a6a7a5b7c0645aa5154b12b1
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-04-02 16:11:59 +0400

    hq-srv

--- all.sh
+++ all.sh
@@ -18,3 +18,89 @@ Authorized access only
 ----------------------
 EOF
 systemctl restart sshd
+apt-get update && apt-get install -y dnsmasq
+cat > /etc/dnsmasq.conf <<EOF
+no-resolv
+no-poll
+no-hosts
+listen-address=192.168.1.10
+
+server=77.88.8.8
+server=195.208.4.1
+server=195.208.5.1
+server=8.8.8.8
+
+cache-size=1000
+all-servers
+no-negcache
+
+host-record=hq-rtr.au-team.irpo,192.168.1.1
+host-record=hq-srv.au-team.irpo,192.168.1.10
+host-record=hq-cli.au-team.irpo,192.168.2.10
+
+address=/br-rtr.au-team.irpo/192.168.3.1
+address=/br-srv.au-team.irpo/192.168.3.10
+
+cname=moodle.au-team.irpo,hq-rtr.au-team.irpo
+cname=wiki.au-team.irpo,hq-rtr.au-team.irpo
+EOF
+systemctl restart dnsmasq
+apt-get install -y chrony
+cat <<EOF > /etc/chrony.conf
+# Use public servers from the pool.ntp.org project.
+# Please consider joining the pool (https://www.pool.ntp.org/join.html).
+#pool pool.ntp.org iburst
+server 192.168.3.10 iburst prefer
+# Record the rate at which the system clock gains/losses time.
+driftfile /var/lib/chrony/drift
+
+# Allow the system clock to be stepped in the first three updates
+# if its offset is larger than 1 second.
+makestep 1.0 3
+
+# Enable kernel synchronization of the real-time clock (RTC).
+rtcsync
+
+# Enable hardware timestamping on all interfaces that support it.
+#hwtimestamp *
+
+# Increase the minimum number of selectable sources required to adjust
+# the system clock.
+#minsources 2
+
+# Allow NTP client access from local network.
+#allow 192.168.0.0/16
+
+# Serve time even if not synchronized to a time source.
+#local stratum 10
+
+# Require authentication (nts or key option) for all NTP sources.
+#authselectmode require
+
+# Specify file containing keys for NTP authentication.
+#keyfile /etc/chrony.keys
+
+# Save NTS keys and cookies.
+ntsdumpdir /var/lib/chrony
+
+# Insert/delete leap seconds by slewing instead of stepping.
+#leapsecmode slew
+
+# Get TAI-UTC offset and leap seconds from the system tz database.
+#leapsectz right/UTC
+
+# Specify directory for log files.
+logdir /var/log/chrony
+
+# Select which information is logged.
+#log measurements statistics tracking
+EOF
+
+
+
+
+
+
+apt-get install chrony -y 
+sed -i '3i#pool pool.ntp.org iburst' /etc/chrony.conf
+systemctl enable --now chronyd
