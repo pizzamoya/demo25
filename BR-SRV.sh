06105a14d3c8c110cbd5a6322227bdc597a9e15e
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-05-30 02:39:51 +0400

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -33,3 +33,80 @@ server 172.16.5.1
 EOF
  set -o history
 systemctl restart chronyd
+cat <<EOF >> /etc/resolv.conf
+nameserver 192.168.1.62
+nameserver 8.8.8.8
+EOF
+apt-get update && apt-get install -y task-samba-dc
+for service in smb nmb krb5kdc slapd bind;
+do
+ systemctl disable $service --now;
+done
+rm -f /etc/samba/smb.conf
+rm -rf /var/lib/samba
+rm -rf /var/cache/samba
+mkdir -p /var/lib/samba/sysvol
+samba-tool domain provision --realm=au-team.irpo --domain=au-team --adminpass='P@ssw0rd' --dns-backend=SAMBA_INTERNAL --option="dns forwarder=77.88.8.8" --server-role=dc
+systemctl enable --now samba
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+systemctl restart samba
+samba-tool domain info 127.0.0.1
+echo "search au-team.irpo" > /etc/net/ifaces/ens18/resolv.conf
+echo "nameserver 127.0.0.1" >> /etc/net/ifaces/ens18/resolv.conf
+echo "nameserver 192.168.1.62" >> /etc/net/ifaces/ens18/resolv.conf
+systemctl restart network
+samba-tool group add hq
+for i in {1..5}; 
+do
+  samba-tool user add user$i.hq P@ssw0rd;
+  samba-tool user setexpiry user$i.hq --noexpiry;
+  samba-tool group addmembers "hq" user$i.hq;
+done
+apt-get install -y admx-*
+admx-msi-setup
+apt-get install -y ansible sshpass
+sed '14i\ inventory = /etc/ansible/hosts' /etc/ansible/ansible.cfg
+sed '15i\ host_key_checking = False' /etc/ansible/ansible.cfg
+cat <<EOF > /etc/ansible/hosts
+HQ-RTR ansible_host=192.168.1.1 ansible_user=net_admin ansible_password=P@ssword ansible_connection=network_cli ansible_network_os=ios
+BR-RTR ansible_host=192.168.0.1 ansible_user=net_admin  ansible_password=P@ssword ansible_connection=network_cli ansible_network_os=ios
+HQ-SRV ansible_host=192.168.1.62 ansible_user=sshuser ansible_password=P@ssw0rd ansible_ssh_port=2024
+HQ-CLI ansible_host=192.168.1.66 ansible_user=user ansible_password=resu
+
+[all:vars]
+ansible_python_interpreter=/usr/bin/python3
+EOF
+#!/bin/bash
+apt-get install -y docker-{engine,compose-v2}
+systemctl enable --now docker.service
+cat <<EOF > wiki.yml
+services:
+  Mediawiki:
+    container_name: wiki
+    image: mediawiki
+    restart: always
+    ports:
+      - 8080:80
+    links:
+      - mariadb
+    volumes:
+      - images:/var/www/html/images
+      # - ./LocalSettings.php:/var/www/html/LocalSettings.php
+
+  mariadb:
+    container_name: mariadb
+    image: mariadb
+    restart: always
+    environment:
+      MYSQL_DATABASE: mediawiki
+      MYSQL_USER: wiki
+      MYSQL_PASSWORD: WikiP@ssw0rd
+      MYSQL_RANDOM_ROOT_PASSWORD: 'yes'
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  images:
+  db:
+EOF
+docker compose -f wiki.yml up -d
