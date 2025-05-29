44d8863f2efba6c5715e3725aaf4842eabfb711c
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-05-29 12:01:30 +0400

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -2,10 +2,10 @@
 history -c 
 set +o history
 apt-get remove -y git
-set -o history
-apt-get update
-apt-get install nano -y 
-set +o history
 cd .. 
 rm -rf demo25
 set -o history
+apt-get update
+apt-get install nano -y 
+
+
