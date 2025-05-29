8e8265e5c024fa17c1606bb5610b6e1be8a04ef2
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-05-29 12:30:02 +0400

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -9,8 +9,8 @@ rm -rf demo25
 set -o history
 apt-get update
 apt-get install nano -y 
+
 set +o history
-mate-terminal --window &
-systemctl status network | sudo tee /dev/pts/1 >/dev/null
+mate-terminal --window -e "bash -c 'sudo systemctl status network; read -p \"Нажмите Enter для выхода...\"'"
 set -o history
 
