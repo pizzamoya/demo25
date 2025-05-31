294906501a9696bb7beb5305edee1ab7c50eb088
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-05-31 19:38:25 +0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get install -y moodle moodle-apache2
+apt-get install -y mariadb-server php8.2-mysqlnd-mysqli
+systemctl enable --now mariadb
+sleep 5
+mariadb -u root -e "CREATE DATABASE moodledb;"
+sleep 5
+mariadb -u root -e "CREATE USER 'moodle'@'%' IDENTIFIED BY 'P@ssw0rd';"
+sleep 5
+mariadb -u root -e "GRANT ALL PRIVILEGES ON moodledb.* TO 'moodle'@'%' WITH GRANT OPTION;"
+sleep 5
+sed -i "s/; max_input_vars = 1000/max_input_vars = 5000/g" /etc/php/8.2/apache2-mod_php/php.ini
+systemctl enable --now httpd2
