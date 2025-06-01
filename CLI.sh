c957ef514b5848a3d068add417b11345d3d36bc2
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-06-01 12:42:11 +0400

    Update CLI.sh

--- CLI.sh
+++ CLI.sh
@@ -181,6 +181,13 @@ cat <<EOF > /etc/resolv.conf
 search au-team.irpo
 nameserver 192.168.0.30
 EOF
+ set +o history
+cat <<EOF > /tmp/test1.txt
+Это после настройки медиавики чтобы скинуть php на br-srv
+scp /home/user/Downloads/LocalSettings.php root@192.168.0.30:/root/docker
+EOF
+cat /tmp/test1.txt
+set -o history
 cat <<EOF > /tmp/ym.txt
 ПОМЕНЯЙТЕ В RESOLV.CONF 
 первый nameserver 192.168.0.30
