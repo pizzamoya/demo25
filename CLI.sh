6500fff1bb0709bf1961d8d68e3fb5cfb20f7854
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-06-01 12:45:07 +0400

    Update CLI.sh

--- CLI.sh
+++ CLI.sh
@@ -184,7 +184,8 @@ EOF
  set +o history
 cat <<EOF > /tmp/test1.txt
 Это после настройки медиавики чтобы скинуть php на br-srv
-scp -P 2024 /home/user/Downloads/LocalSettings.php root@192.168.0.30:/root/docker
+scp -P 2024 /home/user/Downloads/LocalSettings.php sshuser@192.168.0.30:/home/sshuser/
+br-srv# cp /home/sshuser /root/docker
 EOF
 cat /tmp/test1.txt
 set -o history
