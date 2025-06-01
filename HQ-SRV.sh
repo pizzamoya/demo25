d11ad362cdf69ec6a92e4d476a92c87dfe9ff7b9
pizzamoya <151932541+pizzamoya@users.noreply.github.com>
2025-06-01 10:27:36 +0400

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -9,6 +9,7 @@ usermod -aG wheel root
 apt-get remove -y git
 cd .. 
 rm -rf demo25
+timedatectl set-timezone Europe/Samara
 set -o history
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 apt-get update
